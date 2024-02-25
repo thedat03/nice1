be8bcd036ee8ff0a17fbfe427f455a942aac5b35
thedat03 <148042696+thedat03@users.noreply.github.com>
2024-02-25 21:36:47 +0700

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,13 @@
+(function () {
+
+    function playMusic(){
+      var myAudio = document.getElementById('playAudio');
+      if (myAudio.duration > 0 && !myAudio.paused) {
+          //Its playing...do your job
+
+      } else {
+        myAudio.play()
+        //Not playing...maybe paused, stopped or never played.
+
+      }
+    } )
